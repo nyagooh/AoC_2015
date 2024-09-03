6c80088d23ac92ebd60e9c7e59dfa50e969c09ef
nyagooh <annmaina614@gmail.com>
2024-09-03 09:39:48 +0300

    Add day_4 advent of code 2015 which solved a message digest 5 problm

--- day_4/main.go
+++ day_4/main.go
@@ -1,23 +1,44 @@
 package main
 
 import (
-    "crypto/md5"
-    "fmt"
-    "encoding/hex"
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
 )
 
 func main() {
-    secretKey := "bgvyzdsv"
-    number := 1
+	secretKey := "bgvyzdsv"
+	number := 1
+	CryptoHash(secretKey, number)
 
-    for {
-        input := fmt.Sprintf("%s%d", secretKey, number)
-        hash := md5.Sum([]byte(input))
-        hashHex := hex.EncodeToString(hash[:])
-        if hashHex[:5] == "00000" {
-            fmt.Println( number)
-            break
-        }
-        number++
-    }
+}
+
+func CryptoHash(str string, num int) {
+	//global variables
+	var fiveZeroNum, sixZeroNum int
+	foundFive, foundSix := false, false
+	for {
+		//converts str and num to a string and concatenates them
+		input := fmt.Sprintf("%s%d", str, num)
+        //convert the intup to a []byte slice and gets the secret hash code 
+		hash := md5.Sum([]byte(input))
+        //converts the hash code to a secret hexdecimal code
+		hashHex := hex.EncodeToString(hash[:])
+        //checks which number combination and string starts with 5 zereos and prints the combination
+		if !foundFive && hashHex[:5] == "00000" {
+			foundFive = true
+			fiveZeroNum = num
+
+		}
+		if !foundSix && hashHex[:6] == "000000" {
+			foundSix = true
+			sixZeroNum = num
+
+		}
+		if foundFive && foundSix {
+			break
+		}
+		num++
+	}
+fmt.Printf("%d\n%d\n",fiveZeroNum,sixZeroNum)
 }
